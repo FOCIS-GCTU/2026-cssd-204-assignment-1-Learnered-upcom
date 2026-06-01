610e012ac71f80dd26c318a7f44362cd1f0806ad
Learnered-upcom <2425401992@live.gctu.edu.gh>
2026-06-01 07:20:22 -0700

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: <initails.py>
+# Description: <it prints the users initials in his name with the first letters>
+# Assignment Number: <Assignment Number, 1>
+#
+# Name: <ASARE LORD OKYERE>
+# STUDENT ID:  <2425401992>
+# Email: <asarelordokyere@gmail.com>
+# Grader: <AUGUSTUS>
+#
+# On my honor, <ASARE LORD OKYERE>, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+  pass #main function prints the initails
+ print()
+    print("...LAO")
+    print()
+    print("...LLLL...................AAAAAA............OOOOOOOOOO......")
+    print("...LLLL..................AAAAAAAA..........OOOOOOOOOOOO.....")
+    print("...LLLL.................AAAA..AAAA.........OO........OO.....")
+    print("...LLLL................AAAAA..AAAAA........OO........OO.....")
+    print("...LLLL................AAAAAAAAAAAA........OO........OO.....")
+    print("...LLLL................AAAAAAAAAAAA........OO........OO.....")
+    print("...LLLL................AAAA....AAAA........OO........OO.....")
+    print("...LLLLLLLLLLLL........AAAA....AAAA........OO........OO.....")
+    print("...LLLLLLLLLLLL...**...AAAA....AAAA...**...OOOOOOOOOOOO...**")
+    print("...LLLLLLLLLLLL...**...AAAA....AAAA...**....OOOOOOOOOO....**")
 
+    print()
 
 
 main()
